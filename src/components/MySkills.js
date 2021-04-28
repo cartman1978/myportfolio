ec9ea33b697926179db613e7b87c738fc02a006b
Fabio Torti <cartman_7802@yahoo.ie>
2021-04-28 17:23:09 +0100

    added MySkills comnponent

---
+++ src/components/MySkills.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MySkills = () => {
+    return (
+        <div className="services">
+            <h2>See what <span>I can do</span></h2>
+            <h4>Who I am?</h4>
+        </div>
+    )
+}
+
+export default MySkills
