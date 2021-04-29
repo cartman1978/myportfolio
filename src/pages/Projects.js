8c03ba6721b9c1c8223131640c7e7bcdfb766d04
Fabio Torti <cartman_7802@yahoo.ie>
2021-04-29 17:20:52 +0100

    styled Projects component

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -44,10 +44,25 @@ const Projects = () => {
 
 const Work = styled.div`
     min-height: 100vh;
+    overflow: hidden;
+    padding: 5rem 10rem;
+    h2 {
+        padding: 1rem 0rem;
+    }
 `;
 
 const Website = styled.div`
     padding-bottom: 10rem;
+    .line {
+        height: 0.5rem;
+        background: #f7c456;
+        margin-bottom: 3rem;
+    }
+    img {
+        width: 100%;
+        height: 70vh;
+        object-fit: cover;
+    }
 `;
 
 export default Projects;
