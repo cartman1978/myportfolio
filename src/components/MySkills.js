700e172127b3291f9c6704574e3699048542e305
Fabio Torti <cartman_7802@yahoo.ie>
2021-04-29 14:18:59 +0100

    styled MySkills component

--- src/components/MySkills.js
+++ src/components/MySkills.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
+import { About } from "../styles";
 
 const MySkills = () => {
     return (
-        <div className="services">
+        <Service>
             <h2>See what <span>I can do</span></h2>
             <div className="question">
                 <h4>Who I Am?</h4>
@@ -12,6 +14,7 @@ const MySkills = () => {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, laborum!
                 </p>
                 </div>
+                <div className="service-line"></div>
             </div>
             <div className="question">
                 <h4>Responsive Design</h4>
@@ -30,10 +33,41 @@ const MySkills = () => {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, laborum!
                 </p>
                 </div>
+                <div className="service-line"></div>
             </div>
 
-        </div>
+        </Service>
     )
-}
+};
+
+const Service = styled(About)`
+    display: block;
+    span {
+        display: block;
+    }
+    
+    h2 {
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+
+    .service-line {
+      background: #f7c456;
+      height: 0.2rem;
+      margin: 2rem 0rem;
+      width: 100%;
+    }
+    .question {
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+
+    .answer {
+        padding: 2rem 0rem;
+        p {
+            padding: 1rem 0rem;
+        }
+    }
+`;
 
 export default MySkills
