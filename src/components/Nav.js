9b0e1ab86f333b74309e21c8d940f93ec4f2f00f
Fabio Torti <cartman_7802@yahoo.ie>
2021-04-29 14:48:44 +0100

    styled nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Nav = () => {
     return (
         <StyledNav>
-            <h1><a href="#">Dev_Cartman</a></h1>
+            <h1><a id="logo" href="#">Dev_Cartman</a></h1>
             <ul>
                 <li>
                     <a href="#">About me</a>
@@ -25,6 +25,29 @@ const StyledNav = styled.nav`
     display: flex;
     margin: auto;
     justify-content: space-between;
+    align-items: center;
+    padding: 1rem 10rem;
+    background: #282828;
+    a {
+        color: #fff;
+        text-decoration: none;
+    }
+
+    ul {
+        display: flex;
+        list-style: none;
+    }
+
+    #logo{
+        font-size: 1.6rem;
+        font-family: 'Pacifico', cursive;
+        font-weight: lighter;
+    }
+
+    li {
+        padding-left: 10rem;
+        position: relative;
+    }
 `;
 
 export default Nav;
