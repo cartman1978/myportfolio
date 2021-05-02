e2d05c818b1bdf2001db70851145b4e1489b2e1b
Fabio Torti <cartman_7802@yahoo.ie>
2021-05-02 12:39:03 +0100

    added framer motioin in boutSection

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,24 +3,31 @@ import profile from "../img/profile.jpg";
 //Styled
 import styled from "styled-components";
 import { About, Description, Image, Hide } from "../styles";
+//Framer Motion
+import { motion } from 'framer-motion';
 
 const AboutSection = () => {
+
+
     return (
         <About>
             <Description>
-                <div className="title">
+                <motion.div>
+
                     <Hide>
-                        <h2>Hi!</h2>
+                        <motion.h2
+
+                        >Hi!</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>
+                        <motion.h2 >
                             I'm <span>Fabio</span>
-                        </h2>
+                        </motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>a Web Developer</h2>
+                        <motion.h2 >a Web Developer</motion.h2>
                     </Hide>
-                </div>
+                </motion.div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, unde! Dolorem quod nam qui deserunt odit consequuntur accusamus veritatis sed?</p>
                 <button>Contact</button>
             </Description>
