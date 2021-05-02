196c8797324b194e0c5022850a2f125e857b1402
Fabio Torti <cartman_7802@yahoo.ie>
2021-05-02 15:10:35 +0100

    added scale animation to AboutSection

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import { About, Description, Image, Hide } from "../styles";
 //Framer Motion
 import { motion } from 'framer-motion';
+import { titleAnim, fade, photoAnim } from '../animation';
 
 const AboutSection = () => {
 
@@ -16,23 +17,24 @@ const AboutSection = () => {
 
                     <Hide>
                         <motion.h2
-
+                            variants={titleAnim}
                         >Hi!</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2 >
+                        <motion.h2 variants={titleAnim}>
                             I'm <span>Fabio</span>
                         </motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2 >a Web Developer</motion.h2>
+                        <motion.h2 variants={titleAnim}>a Web Developer</motion.h2>
                     </Hide>
                 </motion.div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, unde! Dolorem quod nam qui deserunt odit consequuntur accusamus veritatis sed?</p>
-                <button>Contact</button>
+                <motion.p variants={fade}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, unde! Dolorem quod nam qui deserunt odit consequuntur accusamus veritatis sed?</motion.p>
+                <motion.button variants={fade}>Contact</motion.button>
             </Description>
             <Image>
-                <img src={profile} alt="Fabio Torti" />
+                <motion.img variants={photoAnim} src={profile} alt="Fabio Torti" />
             </Image>
         </About>
     )
