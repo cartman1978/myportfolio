6636ca61fa350da2862448ceb9dd393d2f1cc29c
Fabio Torti <cartman_7802@yahoo.ie>
2021-05-03 17:40:17 +0100

    added animation to contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,20 +1,76 @@
 import React from 'react';
 // Animation
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import { pageAnimation, titleAnim } from '../animation';
+import styled from 'styled-components';
 
 const Contact = () => {
     return (
-        <motion.div
+        <ContactStyle
             exit="exit"
             variants={pageAnimation}
             initial="hidden"
             animate="show"
-            style={{ background: "#fff" }}
+            style={{ background: "#5959f2" }}
         >
-            <h1>Contact Me</h1>
-        </motion.div>
+            <Title>
+                <Hide>
+                    <motion.h2 variants={titleAnim}>Get in touch</motion.h2>
+                </Hide>
+            </Title>
+            <div>
+                <Hide>
+                    <Social variants={titleAnim}>
+                        <Circle />
+                        <h2>Send an Email</h2>
+                    </Social>
+                </Hide>
+
+                <Hide>
+                    <Social variants={titleAnim}>
+                        <Circle />
+                        <h2>LinkedIn</h2>
+                    </Social>
+                </Hide>
+                <Hide>
+                    <Social variants={titleAnim}>
+                        <Circle />
+                        <h2>Instagram</h2>
+                    </Social>
+                </Hide>
+            </div>
+        </ContactStyle>
     );
 };
+
+const ContactStyle = styled(motion.div)` 
+    padding: 5rem 10rem;
+    color: #f7c456;
+    min-height: 90vh;
+`;
+
+const Title = styled.div`
+    margin-bottom: 4rem;
+    color: #f7c456;
+`;
+
+const Hide = styled.div`
+    overflow: hidden;
+`;
+
+const Circle = styled.div`
+    border-radius: 50%;
+    width: 3rem;
+    height: 3rem;
+    background: #f7c456;
+`;
+
+const Social = styled(motion.div)`
+    display: flex;
+    align-items: center;
+    h2 {
+        margin: 2rem;
+    }
+`;
 
 export default Contact;
