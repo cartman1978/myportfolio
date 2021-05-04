56ee18a7ce983e4e96d02b2a55688f95e3b9bde4
Fabio Torti <cartman_7802@yahoo.ie>
2021-05-04 22:04:42 +0100

    added list component from material ui

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
 
 
 // Import Icons
@@ -6,13 +14,30 @@ import webdesign from '../img/webdesign.png';
 import backend from '../img/backend.png';
 import web from '../img/web.jpg';
 import html from '../img/html.png';
+import css from '../img/css.png';
+import sass from '../img/sass.png';
+
 
 // Styles
 import { About, Description, Image } from "../styles";
 import styled from 'styled-components';
 import { scrollReveal } from '../animation';
 import { useScroll } from './useScroll';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        maxWidth: '36ch',
+        backgroundColor: theme.palette.background.paper,
+    },
+    inline: {
+        display: 'inline',
+    },
+}));
+
 const ServicesSection = () => {
+    const classes = useStyles();
     const [element, controls] = useScroll();
     return (
 
@@ -31,20 +56,80 @@ const ServicesSection = () => {
                         </div>
                         <p>I like to code things from scratch, and bringing ideas to life in the browser.</p>
 
-                        <List>
-                            <ul>
-                                <li> <img src={html} alt="html" />HTML</li>
-                                <li>CSS</li>
-                                <li>Sass</li>
-                                <li>Javascript</li>
-                                <li>React</li>
-                                <li>Bootstrap</li>
-                                <li>Material UI</li>
-                            </ul>
+                        <List className={classes.root}>
+                            <ListItem alignItems="flex-start">
+                                <ListItemAvatar>
+
+                                    <img src={html} alt="frontend" />
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary="HTML"
+                                    secondary={
+
+                                        <Typography
+                                            component="span"
+                                            variant="body2"
+                                            className={classes.inline}
+                                            color="textPrimary"
+                                        >
+
+                                        </Typography>
 
 
+                                    }
+                                />
+                            </ListItem>
 
+                            <ListItem alignItems="flex-start">
+                                <ListItemAvatar>
+
+                                    <img src={css} alt="css" />
+
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary="CSS"
+                                    secondary={
+
+                                        <Typography
+                                            component="span"
+                                            variant="body2"
+                                            className={classes.inline}
+                                            color="textPrimary"
+                                        >
+
+                                        </Typography>
+
+
+                                    }
+                                />
+                            </ListItem>
+                            <Divider variant="inset" component="li" />
+                            <ListItem alignItems="flex-start">
+                                <ListItemAvatar>
+                                    <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary="Oui Oui"
+                                    secondary={
+                                        <React.Fragment>
+                                            <Typography
+                                                component="span"
+                                                variant="body2"
+                                                className={classes.inline}
+                                                color="textPrimary"
+                                            >
+                                                Sandra Adams
+                                    </Typography>
+                                            {' — Do you have Paris recommendations? Have you ever…'}
+                                        </React.Fragment>
+                                    }
+                                />
+                            </ListItem>
                         </List>
+
+
+
+
                     </Card>
                     <Card>
                         <div className="icon">
@@ -105,14 +190,7 @@ const Card = styled.div`
     }
 `;
 
-const List = styled.div`
-    ul {
-    list-style: none;
-	margin: 0;
-	padding: 0;
-    }
 
-`;
 
 export default ServicesSection
 
