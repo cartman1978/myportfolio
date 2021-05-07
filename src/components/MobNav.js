ebf66b874c74d1db2bc17317736e16275873ef57
Fabio Torti <cartman_7802@yahoo.ie>
2021-05-07 14:03:03 +0100

    added router to MobNav component

--- src/components/MobNav.js
+++ src/components/MobNav.js
@@ -21,11 +21,11 @@ const MobNav = (props) => {
             </ul>
             <ul id="navItem">
                 <li id="navList">
-                    <Link to="/work">My Work</Link>
+                    <Link to="/projects">Projects</Link>
                     <Line
                         transition={{ duration: 0.75 }}
                         initial={{ width: "0%" }}
-                        animate={{ width: pathname === "/work" ? "50%" : "0%" }}
+                        animate={{ width: pathname === "/projects" ? "50%" : "0%" }}
                     />
                 </li>
             </ul>
